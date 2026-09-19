f01190bcef9b5f771418c7ebb1fd87d0c7d24264
stester101 <85431356+stester101@users.noreply.github.com>
2026-09-19 21:06:31 +0100

    Bump FlipFit to v0.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.simontester.flipfit"
         minSdk = 28
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.5.0"
+        versionCode = 7
+        versionName = "0.6.0"
     }
 
     compileOptions {
